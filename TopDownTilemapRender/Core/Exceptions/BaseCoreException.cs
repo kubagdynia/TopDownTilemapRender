f22b243e422dbc618289708d23e6e3cb07c503a6
Jakub Kurłowicz <kubagdynia@gmail.com>
2018-04-30 21:38:38 +0200

    Added core base exceptions

---
+++ TopDownTilemapRender/Core/Exceptions/BaseCoreException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopDownTilemapRender.Core.Exceptions
+{
+    public class BaseCoreException : Exception
+    {
+        protected BaseCoreException(string message, string hint)
+            : base(hint != null ? ($"{message}\n{hint}") : message)
+        {
+
+        }
+    }
+}
